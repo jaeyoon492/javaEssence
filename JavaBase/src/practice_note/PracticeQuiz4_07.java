1f302f50f05b6401b9e2d476fc997f8f97d8be18
JaeYoonMyung <77507968+jaeyoon492@users.noreply.github.com>
2021-02-23 00:15:57 +0900

    PracticeJava

--- JavaBase/src/practice_note/PracticeQuiz4_07.java
+++ JavaBase/src/practice_note/PracticeQuiz4_07.java
@@ -1,15 +1,14 @@
 package practice_note;
 
-import java.util.Arrays;
-
 public class PracticeQuiz4_07 {
 	public static void main(String[] args) {
-		String str1 = "a";
-		String str2 = "aa";
-		String str3 = "aaa";
-		String str4 = "aaaa";
-		String str5 = "aaaaa";
-		String str6 = "aaaaaa";
+		
+		//String str1 = "a";
+		//String str2 = "aa";
+		//String str3 = "aaa";
+		//String str4 = "aaaa";
+		//String str5 = "aaaaa";
+		//String str6 = "aaaaaa";
 		
 		//String의 method - length() >>> 반환타입은 int로 문자열이 몇개의 문자로 이루어져 있는지 반환해준다.
 //		System.out.println(str6.length());
@@ -18,6 +17,7 @@ public class PracticeQuiz4_07 {
 		int sum = 0;
 		
 		for(int i = 0 ; i < str.length() ; i++ ) {
+			System.out.println(i);
 			sum += (int)str.charAt(i)-'0';
 		}
 		
---
+++ JavaBase/src/practice_note/PracticeQuiz_5_02.java
@@ -0,0 +1,15 @@
+package practice_note;
+import java.util.Arrays;
+public class PracticeQuiz_5_02 {
+
+	public static void main(String[] args) {
+		 int[][] arr= {
+				 {5,5,5,5,5},
+				 {10,10,10},
+				 {20,20,20,20},
+				 {30,30}
+				};
+		 
+				System.out.println(Arrays.toString(arr[3]));
+	}
+}
