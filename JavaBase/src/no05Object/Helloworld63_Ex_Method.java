ce1cbcc247ed29c25d6ead4f54f4ac581e5a0377
JaeYoonMyung <77507968+jaeyoon492@users.noreply.github.com>
2021-02-27 04:09:06 +0900

    Update Helloworld63_Ex_Method.java

--- JavaBase/src/no05Object/Helloworld63_Ex_Method.java
+++ JavaBase/src/no05Object/Helloworld63_Ex_Method.java
@@ -3,11 +3,11 @@ package no05Object;
 public class Helloworld63_Ex_Method {
 
 	public static void main(String[] args) {
-		MyMath mm = new MyMath();		//클래스 MyMath를 mm에 저장
-		long result1 = mm.add(5L, 3L);	//result1에 mm의 add메서드의 반환값을 저장
-		long result2 = mm.subtract(5L, 3L);
-		long result3 = mm.multiply(5L, 3L);
-		double result4 = mm.divide(5L, 3L);
+		MyMath mm = new MyMath();			//MyMath(객체) 인스턴스를 참조변수 mm에 저장
+		long result1 = mm.add(5L, 3L);		//add메서드를 호출하여 result1에 저장
+		long result2 = mm.subtract(5L, 3L);	//subtract메서드를 호출하여 result2에 저장
+		long result3 = mm.multiply(5L, 3L);	//multiply메서드를 호출하여 result3에 저장
+		double result4 = mm.divide(5L, 3L);	//divide메서드를 호출하여 result4에 저장
 		
 		System.out.println("add(5L, 3L) = " +result1);
 		System.out.println("subtract(5L, 3L) = "+ result2);
