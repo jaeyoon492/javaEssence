2411c1822552b18b98b0f47b6507cae1fa359f12
JaeYoonMyung <77507968+jaeyoon492@users.noreply.github.com>
2021-03-04 12:29:29 +0900

    임시 커밋

--- JavaBase/src/no05Object/Helloworld71_iv_this.java
+++ JavaBase/src/no05Object/Helloworld71_iv_this.java
@@ -2,7 +2,7 @@ package no05Object;
 
 public class Helloworld71_iv_this {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("인텔리제이Z");
     }
 
 }
